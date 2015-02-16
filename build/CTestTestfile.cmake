b12cd3d31478760ea695cfd4fb14e7a05288b620
Alexandre Rupp <alexandre.rupp@ensimag.grenoble-inp.fr>
2015-02-16 19:40:28 +0100

    Delete content of build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexandre/documents/cours/2A/SEPC/sepc_tp1
-# Build directory: /home/alexandre/documents/cours/2A/SEPC/sepc_tp1/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(AllTestsAllocator "alloctest")
-SUBDIRS(gtest)
